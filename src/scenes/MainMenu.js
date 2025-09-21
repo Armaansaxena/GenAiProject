02bcfacd6a8b01f961253e39a3315332db13db06
[Armaansaxena] <armaansaxena704@gmail.com>
2025-09-21 21:19:20 +0530

    Updated Main menu file

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -21,9 +21,7 @@ export class MainMenu extends Scene {
             this.showInstructions();
         });
 
-        this.createButton(centerX, startY + buttonSpacing * 2, 'Support Philoagents', () => {
-            window.open('https://github.com/neural-maze/philoagents-course', '_blank');
-        });
+        
     }
 
     createButton(x, y, text, callback) {
